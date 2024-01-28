237a933ee65b75479301af9810979376f7eb894a
valmon55 <fedor_ka@mail.ru>
2024-01-28 23:00:46 +0300

    interface IUnitOfWork

---
+++ ASP/ASP.SocialNetwork/Models/DAL/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ASP.SocialNetwork.Models.DAL
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        int SaveChanges(bool ensureAutoHistory = false);
+        IRepository<TEntity> GetRepository<TEntity>(bool hasCustomRepository = true) where TEntity : class;
+
+    }
+}
